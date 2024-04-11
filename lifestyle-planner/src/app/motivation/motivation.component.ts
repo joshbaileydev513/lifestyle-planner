958cbfdd625d7c5ea7213ec131012c2df142c947
Joshua Bailey <joshbaileydev513@gmail.com>
2024-04-11 13:01:54 -0400

    added login forms/styling along with updated motivation to show scss

--- lifestyle-planner/src/app/motivation/motivation.component.ts
+++ lifestyle-planner/src/app/motivation/motivation.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-motivation',
   templateUrl: './motivation.component.html',
-  styleUrls: ['./motivation.component.css']
+  styleUrls: ['./motivation.component.scss']
 })
 export class MotivationComponent {
   // Add any logic for your motivation component here
