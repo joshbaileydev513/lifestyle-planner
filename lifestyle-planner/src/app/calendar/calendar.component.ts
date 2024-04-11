33bd9eeebfc103ccb09258bbc3a57e3c75e3c173
Joshua Bailey <joshbaileydev513@gmail.com>
2024-04-11 12:43:35 -0400

    finally got my bug figured out with the stylesheet not confirguring in webpack, was an issue with my styles component within each .ts

--- lifestyle-planner/src/app/calendar/calendar.component.ts
+++ lifestyle-planner/src/app/calendar/calendar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent {
   // Add any logic for your calendar component here
