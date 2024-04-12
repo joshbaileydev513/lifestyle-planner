47dc0d6f5e1f934b9099ad331657bebc0e8006d1
Joshua Bailey <joshbaileydev513@gmail.com>
2024-04-12 11:48:26 -0400

    attempting to perform an api request for motivational quotes to display. have  CORs error that I am working through.

--- lifestyle-planner/src/app/motivation/motivation.component.ts
+++ lifestyle-planner/src/app/motivation/motivation.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuotesService } from '../services/quotes.service';
 
 @Component({
   selector: 'app-motivation',
   templateUrl: './motivation.component.html',
   styleUrls: ['./motivation.component.scss']
 })
-export class MotivationComponent {
-  // Add any logic for your motivation component here
+
+export class MotivationComponent implements OnInit {
+  quotes: any[] = [];
+
+  constructor(private quotesService: QuotesService) { }
+
+  ngOnInit(): void {
+    this.loadQuotes();
+  }
+
+  loadQuotes(): void {
+    this.quotesService.getQuotes().subscribe({
+      next: (data) => {
+        this.quotes = data;
+        console.log(data); // Optional: to see data in the console
+      },
+      error: (err) => console.error('Error fetching quotes:', err),
+      complete: () => console.log('Quotes fetch completed') // Optional
+    });
+  }
 }
